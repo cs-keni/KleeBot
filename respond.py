3e7449bc0411559258a0c676a0f864a1fc2a4dae
cs-keni <keenyuyu@gmail.com>
2023-07-27 18:46:52 -0700

    cleaned up main and organized voicelines and emojis into separate file

--- respond.py
+++ respond.py
@@ -1,3 +1,4 @@
 ohayo = "Spark Knight Klee of the Knights of Favonius, reporting for duty!"
 sayonara = "Spark Knight Klee of the Knights of Favonius, signing off!"
 no_perms = "You do not have permission to use this command!"
+unknown_weather = "What's this? This is the first I've seen this type of weather!"
---
+++ weather_data.py
@@ -0,0 +1,35 @@
+weather_voicelines = {
+    "Clear" : "Oh, it's sunny outside?! Yay! That means the perfect weather for my Boom-Boom adventures!",
+    "Rain" : "Mwauhahaha, lucky all my new bombs are waterproof!",
+    "Clouds" : "Clouds, clouds, fluffy clouds! You know, they look just like the cotton candy I had at the Windblume Festival!",
+    "Thunderstorm" : "What'd I blow up!? ...Oh wait, it's just thunder and lightning. Phew.",
+    "Snow" : "I've got the bestest idea ever! Let's build a Dodoco snowman family! We'll start with Dodoco Dad, then Dodoco Mom, then Dodoco Baby, then... ah... ah.. achoo!",
+    "Drizzle" : "Hey! Hey! Did you know? If I throw a bomb into the rain, when it explodes, it'll make a white rainbow! I call it a boom-bow!",
+    "Mist" : "Misty days are like stepping into a dream! I'll use my Mystical Glitterbombs to make the mist shimmer and glow like stars in the sky!",
+    "Haze" : "Haze makes me think of stories of Big Sister Jean's and Grandpa Varka's adventures! I would go on adventures too, but Big Sister Jean says I'm too young. Hmph.",
+    "Fog" : "Ehhh..?! Everything is so blurry! No.. No! It can't be! Am I already turning old?! Noooooo!",
+    "Smoke" : "Heh, my bombs are way more smoky than this! I'll show you! :bomb: :boom:",
+    "Dust" : "Pft, this amount of dust is nothing compared to the dust I make when I blow up a hilichurl camp!",
+    "Sand" : "What the heck?! Are we in the deserts of Sumeru?! Ack! It got in my mouth! Blegh!! :confounded:",
+    "Ash" : "Wow, it's ashy outside? Did a volcano erupt in Natlan or something?",
+    "Squall" : "Dear Anemo God, please make Klee's bombs blow in the right direction and only blow up bad guys. The end.",
+    "Tornado" : "Fwuah!! Did I accidently make the Anemo God mad with my bombs?! I'm sorry!!"
+}
+
+weather_emojis = {
+    "Clear" : ":sunny:",
+    "Rain" : ":cloud_rain:",
+    "Clouds" : ":cloud:",
+    "Thunderstorm" : ":thunder_cloud_rain:",
+    "Snow" : ":cloud_snow:",
+    "Drizzle" : ":cloud_rain:",
+    "Mist" : ":fog:",
+    "Haze" : ":fog:",
+    "Fog" : ":fog:",
+    "Smoke" : ":fog:",
+    "Dust" : ":fog:",
+    "Sand" : ":fog:",
+    "Ash" : ":fog:",
+    "Squall" : ":wind_blowing_face:",
+    "Tornado" : ":cloud_tornado:"
+}
